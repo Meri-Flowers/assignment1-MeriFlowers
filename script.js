f4b9e5083aaca1fa9f8644e92700aed996aebb85
Meri-Flowers <meri.flowers@msj.edu>
2026-05-03 19:36:51 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Show the feedback prompt once the user scrolls to the bottom of the page.
 window.addEventListener('scroll', function() {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
         document.getElementById('feedback').showModal();
@@ -5,26 +6,30 @@ window.addEventListener('scroll', function() {
     }
 });
 
-
+//Wait until the page is loaded before attaching button and form event listeners.
 document.addEventListener('DOMContentLoaded', function() {
+    //  "Love" reaction button click.
     document.getElementById('love-btn').addEventListener('click', function() {
         alert('Yeehaw! Our Cat Boys are amazing!');
         console.log('Love reaction');
         document.getElementById('feedback').close();
     });
 
+    //"Like" reaction button click.
     document.getElementById('like-btn').addEventListener('click', function() {
         alert('Yeehaw! Our Cat Boys are alright!');
         console.log('Like reaction');
         document.getElementById('feedback').close();
     });
 
+    //"Dislike" reaction button click.
     document.getElementById('dislike-btn').addEventListener('click', function() {
         alert('Yeehaw! Our Cat Boys don\'t like you either!');
         console.log('Dislike reaction');
         document.getElementById('feedback').close();
     });
 
+    // button that closes the exits the prompt without response.
     document.getElementById('no-feedback-btn').addEventListener('click', function() {
         document.getElementById('feedback').close();
     });
@@ -35,7 +40,6 @@ document.addEventListener('DOMContentLoaded', function() {
         quoteForm.addEventListener('submit', function(event) {
             event.preventDefault();
 
-            
             var formData = {
                 name: document.getElementById('name').value,
                 email: document.getElementById('email').value,
@@ -46,4 +50,5 @@ document.addEventListener('DOMContentLoaded', function() {
             quoteForm.reset();
         });
     }
+
 });
